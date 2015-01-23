b6829fb126f923fbcbbc902783c5eaefd0f5d8b8
Spencer Elliott <me@elliottsj.com>
2015-01-23 14:23:15 -0500

    Use 2 spaces instead of 4

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,21 @@
 
 module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    grunt.initConfig({
-        nodewebkit: {
-            options: {
-                platforms: ['win','osx', 'linux'],
-                buildDir: './build'  // Where the build version of your nw.js app is saved
-            },
-            src: ['./app/**/*']  // Your node-webkit app
-        }
-    });
+  grunt.initConfig({
+    nodewebkit: {
+      options: {
+        platforms: ['win', 'osx', 'linux'],
+        buildDir: './build'  // Where the build version of your nw.js app is saved
+      },
+      src: ['./app/**/*']  // Your node-webkit app
+    }
+  });
 
-    grunt.registerTask('build', ['nodewebkit']);
+  grunt.registerTask('build', ['nodewebkit']);
 };
