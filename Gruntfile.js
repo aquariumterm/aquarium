0e547d5aa25e1cead32acab434084d52d7b5ca5f
Spencer Elliott <me@elliottsj.com>
2015-01-23 15:12:03 -0500

    Add artifact compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,68 @@ module.exports = function (grunt) {
         buildDir: './build'  // Where the build version of your nw.js app is saved
       },
       src: ['./app/**/*']  // Your node-webkit app
+    },
+    compress: {
+      options: { pretty: true },
+      linux32: {
+        options: {
+          archive: 'build/compressed/linux32.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['linux32/**'],
+        dest: ''
+      },
+      linux64: {
+        options: {
+          archive: 'build/compressed/linux64.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['linux64/**'],
+        dest: ''
+      },
+      osx32: {
+        options: {
+          archive: 'build/compressed/osx32.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['osx32/**'],
+        dest: ''
+      },
+      osx64: {
+        options: {
+          archive: 'build/compressed/osx64.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['osx64/**'],
+        dest: ''
+      },
+      win32: {
+        options: {
+          archive: 'build/compressed/win32.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['win32/**'],
+        dest: ''
+      },
+      win64: {
+        options: {
+          archive: 'build/compressed/win64.zip'
+        },
+        expand: true,
+        cwd: 'build/frosh/',
+        src: ['win64/**'],
+        dest: ''
+      }
     }
   });
 
-  grunt.registerTask('build', ['nodewebkit']);
+  grunt.registerTask('build', [
+    'nodewebkit',
+    'compress'
+  ]);
 };
