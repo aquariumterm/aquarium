ad33b8024c940cef8f9ebb67af3f26b76e54a48d
Spencer Elliott <me@elliottsj.com>
2015-01-23 13:51:42 -0500

    Configure build

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        nodewebkit: {
+            options: {
+                platforms: ['win','osx', 'linux'],
+                buildDir: './build'  // Where the build version of your nw.js app is saved
+            },
+            src: ['./app/**/*']  // Your node-webkit app
+        }
+    });
+
+    grunt.registerTask('default', [
+        'nodewebkit'
+    ]);
+};
