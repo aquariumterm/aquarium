37247703a25518b2922b4fee0446a8af282a7969
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-01-27 00:08:35 -0500

    Configure more testing stuff to set up a Selenium server

--- app/test/test.js
+++ app/test/test.js
@@ -1,10 +1,13 @@
 var wd = require('wd');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
+var chromedriver = require('chromedriver');
 
 // initialize chai
 chai.use(chaiAsPromised);
 chai.should();
+
+chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 
 describe("using promises and chai-as-promised", function() {
   var browser;
@@ -12,18 +15,22 @@ describe("using promises and chai-as-promised", function() {
   before(function() {
     browser = wd.promiseChainRemote();
 
+    chromedriver.start();
     return browser.init({browserName:'chrome'});
   });
 
   beforeEach(function() {
-    return browser.get("http://localhost:8081/");
+    return browser.get("http://localhost:8081");
   });
 
-  after(function() {
-    return browser.quit();
+  after(function(done) {
+    browser.quit(function() {
+      chromedriver.stop();
+      done();
+    });
   });
 
   it("should retrieve the page title", function() {
-    return browser.title().should.become("Hello World");
+    return browser.title().should.become("Hello World!");
   });
 });
