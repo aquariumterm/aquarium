5572857818be61ec04da7e657cec378e3efb9205
Nick <contact@nicholasgoh.me>
2015-03-05 02:30:50 -0500

    Updated to display placeholder entries

--- app/js/components/Sidebar.js
+++ app/js/components/Sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// import Entry from './Entry';
+
 var Sidebar = React.createClass({
   render: function() {
     return (
@@ -7,6 +9,23 @@ var Sidebar = React.createClass({
       	<form action =''>
         	<input type="text" name="query" placeholder="Search"></input>
       	</form>
+
+      	<Entry command="ls">List directory contents</Entry> 	
+      	<Entry command="pwd">Print the name of current/working directory</Entry>
+      </div>
+    );
+  }
+});
+
+var Entry = React.createClass({
+  render: function() {
+    return (
+      <div className="Entry">
+      	<hr></hr>
+        <h2 className="entryCommand">
+          {this.props.command}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
